7a0e9b68f691438954d2e2c8f77b04c464b01f51
Said Ali <saidibnali19@gmail.com>
2023-08-07 18:34:14 +0300

    feat: created xhr

---
+++ xhr.js
@@ -0,0 +1,10 @@
+const XMLHttpRequest = require('xhr2');
+
+const xhr = new XMLHttpRequest();
+xhr.onreadystatechange = () => {
+  if (xhr.readyState === 4) {
+    xhr.status === 200 ? console.log(xhr.responseText) : console.error('error');
+  }
+};
+xhr.open('GET', 'https://react.dev');
+xhr.send();
